420365cd12cfedf16cbc7a750178671bb5fc0ab1
Igorx945 <128699017+Igorx945@users.noreply.github.com>
2024-02-28 15:38:03 -0300

    ok

--- exsoma.js
+++ exsoma.js
@@ -1,18 +1,1 @@
-var num1= 23;var num2= -12;
-// console.log(num1+num2)
-
-var soma;
-var resultado;
-
-function calcularSoma (num1, num2) {
-  soma = num1+num2
-  return soma
-}
-resultado = calcularSoma(num1, num2);   
-console.log('A soma de ', num1, '+', num2, '=',resultado);
-
-num1 = 159
-num2 = 249
-
-resultado = calcularSoma(num1, num2);
-console.log('A soma é:',resultado);
+console.log('ola, mundo');
